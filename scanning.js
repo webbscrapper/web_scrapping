40534b578b4583b1167d9c431615a80d5c15ab2f
webbscrapper <146927677+webbscrapper@users.noreply.github.com>
2023-11-10 20:36:56 +0500

    Update scanning.js

--- scanning.js
+++ scanning.js
@@ -641,7 +641,7 @@ try {
           dicountpercentage: discountpercent,
 
         });
-        if(discountpercent >= discountpercentagee && price > minprice ){
+        if(discountpercent >= discountpercentagee && newprice > minprice ){
          
             sendDataInEmail.push({
               image: productData[i].imageurl,
